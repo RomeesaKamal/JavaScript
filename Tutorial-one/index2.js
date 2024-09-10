e42b1c9c962e30eebf46795dce54795821b800ca
RomeesaKamal <kamalromeesa@gmail.com>
2024-09-10 11:26:10 +0500

    add all files

---
+++ Tutorial-one/index2.js
@@ -0,0 +1,10 @@
+// variable  = A container that stores a value.
+//             Behaves as if it were the value it contain .
+
+// 1. declaration    let x ;
+// 2. assignment     x = 100
+
+let x;
+x = 120;
+
+console.log("x");
---
+++ index.js
@@ -0,0 +1,12 @@
+console.log(`Hello`);
+console.log(`I Like Pizza`);
+
+window.alert(`This is an alert!`)
+window.alert(`Hello World!`)
+
+//this is a comment
+
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myp").textContent = `I Like Pizza`;
+
