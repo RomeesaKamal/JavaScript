cc877ad08c2f0c31f0a93a8c1d5f8e1112eb63d3
RomeesaKamal <kamalromeesa@gmail.com>
2024-09-11 12:13:55 +0500

    add all files

---
+++ Tutorial-two/index3.js
@@ -0,0 +1,15 @@
+// arithmetic operators = operands (values, variable, etc)
+ //                       operators (+ - */)
+ //                       ex. 11 = x + 5
+ let students = 30;
+
+  students = students + 1
+//   students = students - 2
+//   students = students * 2
+//   students = students / 2
+//   students = students **2
+  students = students %2
+
+
+
+ console.log(students)
