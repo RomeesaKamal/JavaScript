ac6d38b3228e8bcf0f9332d5589f6544ee978087
RomeesaKamal <kamalromeesa@gmail.com>
2024-09-13 12:11:42 +0500

    add Tutorail-three folder

--- Tutorial-two/index3.js
+++ Tutorial-two/index3.js
@@ -1,15 +1,38 @@
 // arithmetic operators = operands (values, variable, etc)
  //                       operators (+ - */)
  //                       ex. 11 = x + 5
- let students = 30;
+//  let students = 30;
 
-  students = students + 1
+  // students = students + 1
 //   students = students - 2
 //   students = students * 2
 //   students = students / 2
 //   students = students **2
-  students = students %2
+  // students = students %2
 
+// students += 4;
+// students -= 8;
+// students *= 2;
+// students *= 2;
+// students /= 2;
+// students **= 2;
+// students %= 2;
 
+// students++;
+// students--;
 
- console.log(students)
+//  console.log(students)
+
+ /*
+  opertaor precedence
+ 1. parenthesis
+ 2. exponent
+ 3. multiplication & division & modlo
+ 4. addition & substraction 
+*/
+
+// let result = 1 + 2 * 3 + 4 ** 3
+// let result = 12 % 5 + 8 / 2;
+let result = 6 / 2 ** (2 +5); /* 2+5=7, 2*7=128, 6/128=0.046875*/
+
+console.log(result);
