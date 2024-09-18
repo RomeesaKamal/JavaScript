866703bf0d4807da296a940c35b75f0b173e23f6
RomeesaKamal <kamalromeesa@gmail.com>
2024-09-18 21:02:03 +0500

    add the files

--- fourth.js
+++ fourth.js
@@ -10,6 +10,14 @@
 // const PI = 3.14;
 // console.log(PI);
 
-for (let count = 1; count <= 5; count++){
-    console.log("Romeesa Kamal");
+// for (let count = 1; count <= 100000; count++){
+//     console.log("Romeesa Kamal");
+// }
+// console.log("loop has ended");
+
+let sum = 0;
+for (let i = 1; i = 50; i++){
+    sum = sum + i;
 }
+console.log(sum);
+console.log("loop has ended");
