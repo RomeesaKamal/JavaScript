ced8d8e43c0ff26c7d0e4883e037674eb1594135
RomeesaKamal <kamalromeesa@gmail.com>
2024-09-21 22:35:12 +0500

    correction in files

---
+++ fourth.js
@@ -0,0 +1,87 @@
+// let gameNum = 25;
+
+// let userNum = prompt("Guess the game number :");
+
+// while(userNum != gameNum){
+    // userNum = prompt("you entered wrong number.Guess again :");
+
+// }
+// console.log("congratualations,you enterd the right number");
+
+//strings:
+// let str = "RomeesaKamal";
+// console.log(str[11]);
+
+// Templates literal:
+// let obj = {
+    // item : "pen",
+    // price : 10,
+// }
+// let output = `the cost of ${obj.item} is ${obj.price} rupees`
+// console.log(output);
+
+// let specialString = `thos is a template string ${1+2+3}`;
+// console.log(specialString);
+
+// console.log("Apna  \t College");
+// let str = "Apna\nCollege"
+// console.log(str.length)
+
+// variable  = A container that stores a value.
+//             Behaves as if it were the value it contain .
+
+// 1. declaration    let x ;
+// 2. assignment     x = 100
+
+// let x = 230;
+
+// console.log(x);
+
+// let age = 25;
+
+// console.log(age);
+
+// Fullname = "Romeesa Kamal";
+// console.log(Fullname);
+// x = null;
+// y = undefined;
+// console.log(x)
+// console.log(y)
+
+// {
+//     let a = 5
+//     console.log(a)
+// }
+
+// let str = "rommesakamal"
+//  str = str.toUpperCase();
+//  str = str.toLowerCase();
+// console.log(str);
+
+// let str = "    Romeesa kamal     JS       "
+// console.log(str.trim());
+
+// let str = "012345678"
+// let str = "hello"
+// console.log(str.slice(1, 3));// ending index is non inclusive 
+
+// let str1 = "Apna"
+// let str2 = "college"
+// let res = str1.concat(str2)
+// let res = "I am learning coding from" + str1 + str2;
+// console.log(res);
+
+// let str = "Hello";
+
+// console.log(str.replace("lo", "p"));
+
+// let str = "IloveJS";
+// console.log(str.charAt(6));
+
+
+let fullName = prompt("Enter your fullname without spaces");
+
+let userName ="@" + fullName + fullName.length;
+
+console.log(userName);
+
